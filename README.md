ec2044cadbc4aabe00375bc8d0753413712d05e9
Bingxing Wang <i@imbushuo.net>
2018-05-13 06:17:51 -0700

    Lumia950XLPkg: Ready for publish.

---
+++ README.md
@@ -0,0 +1,54 @@
+# EDK2 Implementation for Lumia 950 XL
+
+## What's this?
+
+This package demonstrates an AArch64 UEFI implementation for hacked Lumia 950 XL. 
+Currently it is able to boot Windows 10 ARM64 (with a minor patch).
+Booting Linux is also possible if seven cores are disabled in ACPI tables (due to the TZ implementation).
+
+## Build
+
+If you are familar with EDK2, you don't need to use my build script.
+
+- Checkout a copy of [EDK2](https://github.com/tianocore/edk2), then checkout this repository under EDK2's worktree.
+- Install [Linaro AArch64 GCC toolchains](http://releases.linaro.org/components/toolchain/binaries/), my build
+script uses `gcc-linaro-7.2.1-2017.11`. Then untar them. I place everything under `/opt` directory, so I have 
+directories like `/opt/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-elf/bin`. If you placed it somewhere else, modify build
+scripts.
+- Copy `rundbbuild.sh` in `Tools` directory to your EDK2 worktree root directory.
+- Export variable `GCC5_AARCH64_PREFIX` to your GCC directory with prefix (e.g. `/opt/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-elf/bin/aarch64-elf-`)
+- Start build: `. rundbbuild.sh --950xl --development`
+
+## Run
+
+It requires a kickstarter to run. Public details soon.
+
+## Patch for booting Windows ARM64
+
+Go to your EDK2 worktree, find `MdePkg/Include/AArch64/ProcessorBind.h`:
+
+	- #define RUNTIME_PAGE_ALLOCATION_GRANULARITY (0x10000)
+	+ #define RUNTIME_PAGE_ALLOCATION_GRANULARITY (0x1000)
+
+This issue will be addressed with improved memory allocation in the future.
+
+## TZ Implementation Notes
+
+Qualcomm Snapdragon MSM8992/MSM8994 implements a subset of [PSCI interface](http://infocenter.arm.com/help//topic/com.arm.doc.den0022d/Power_State_Coordination_Interface_PDD_v1_1_DEN0022D.pdf) for multi-processor startup. However, required
+commands like `PSCI_SYSTEM_OFF` and `PSCI_SYSTEM_RESET` are not implemented. Hence we use PMIC to shutdown
+platform (there's a bug in RT that will be fixed) instead of PSCI. Additionally, 8992/8994 uses HVC call for
+PSCI commands instead of SMC call. It seems that Linux encounters some troubles during MP startup. 
+
+## Linux Notes
+
+The ACPI tables are copied from stock Windows Phone FFU, hence these device IDs are likely not be recognized by Linux.
+A proper device tree is still required to boot Linux. If you are interested, you are welcome to contribute.
+
+# Acknowledgements
+
+- [EFIDroid Project](http://efidroid.org)
+
+# License
+
+All code except drivers in `GPLDriver` directory are licensed under BSD 2-Clause. 
+GPL Drivers are licensed under GPLv2 license.
---
+++ Tools/rundbbuild.sh
@@ -0,0 +1,223 @@
+#!/bin/bash
+
+SCRIPTNAME="rundbbuild.sh"
+RECONFIG=FALSE
+DEVBUILD=FALSE
+CIBUILD=FALSE
+DB820C=FALSE
+LUMIA950XL=FALSE
+
+function HelpMsg()
+{
+  echo "Usage: $SCRIPTNAME [Options]"
+  echo
+  echo "Configure EDK2 build environment, then kicks off build for Dragonboard 410c."
+  echo
+  echo
+  echo "Options: "
+  echo "  --help, -h, -?        Print this help screen and exit."
+  echo
+  echo "  --development, -dev   Run development build (dirty)."
+  echo
+  echo "  --820c, -820c         Run build for Dragonboard 820c."
+  echo
+  echo "  --950xl, -950xl       Run build for Lumia 950 XL."
+  echo
+  echo "  --production, -ci     Run CI build (clean)."
+  echo
+}
+
+function SetWorkspace()
+{
+  #
+  # If WORKSPACE is already set, then we can return right now
+  #
+  if [ -n "$WORKSPACE" ]
+  then
+    return 0
+  fi
+
+  #
+  # Check for BaseTools/BuildEnv before dirtying the user's environment.
+  #
+  if [ ! -f BaseTools/BuildEnv ] && [ -z "$EDK_TOOLS_PATH" ]
+  then
+    echo BaseTools not found in your tree, and EDK_TOOLS_PATH is not set.
+    echo Please point EDK_TOOLS_PATH at the directory that contains
+    echo the EDK2 BuildEnv script.
+    return 1
+  fi
+
+  #
+  # Set $WORKSPACE
+  #
+  export WORKSPACE=`pwd`
+
+  return 0
+}
+
+function SetupEnv()
+{
+  if [ -n "$EDK_TOOLS_PATH" ]
+  then
+    . $EDK_TOOLS_PATH/BuildEnv
+  elif [ -f "$WORKSPACE/BaseTools/BuildEnv" ]
+  then
+    . $WORKSPACE/BaseTools/BuildEnv
+  elif [ -n "$PACKAGES_PATH" ]
+  then 
+    PATH_LIST=$PACKAGES_PATH
+    PATH_LIST=${PATH_LIST//:/ }
+    for DIR in $PATH_LIST
+    do
+      if [ -f "$DIR/BaseTools/BuildEnv" ]
+      then
+        export EDK_TOOLS_PATH=$DIR/BaseTools
+        . $DIR/BaseTools/BuildEnv
+        break
+      fi
+    done
+  else
+    echo BaseTools not found in your tree, and EDK_TOOLS_PATH is not set.
+    echo Please check that WORKSPACE or PACKAGES_PATH is not set incorrectly
+    echo in your shell, or point EDK_TOOLS_PATH at the directory that contains
+    echo the EDK2 BuildEnv script.
+    return 1
+  fi
+}
+
+function FixPermission()
+{
+  chmod +x DragonboardPkg/Tools/*.ps1
+}
+
+function SourceEnv()
+{
+  SetWorkspace &&
+  SetupEnv &&
+  FixPermission
+}
+
+function DevelopmentBuild()
+{
+  if [ "$DB820C" = TRUE ]; then
+    ./Dragonboard820cPkg/Tools/edk2-build.ps1
+  elif [ "$LUMIA950XL" = TRUE ]; then
+    ./Lumia950XLPkg/Tools/edk2-build.ps1 -UseNewerGcc
+  else
+    ./DragonboardPkg/Tools/edk2-build.ps1
+  fi
+
+  if [ ! $? -eq 0 ]; then
+      echo "[Builder] Build failed."
+      return $?
+  fi
+
+  if [ "$DB820C" = TRUE ]; then
+    ./Dragonboard820cPkg/Tools/build-lkimage.ps1
+  elif [ "$LUMIA950XL" = TRUE ]; then
+    ./Lumia950XLPkg/Tools/build-lkimage.ps1
+  else
+    ./DragonboardPkg/Tools/build-lkimage.ps1
+  fi
+  
+}
+
+function CIBuild()
+{
+  if [ "$DB820C" = TRUE ]; then
+    ./Dragonboard820cPkg/Tools/edk2-build.ps1 -Clean
+  elif [ "$LUMIA950XL" = TRUE ]; then
+    ./Lumia950XLPkg/Tools/edk2-build.ps1 -Clean -UseNewerGcc
+  else
+    ./DragonboardPkg/Tools/edk2-build.ps1 -Clean
+  fi
+
+  if [ ! $? -eq 0 ]; then
+      echo "[Builder] Build failed."
+      return $?
+  fi
+
+  if [ "$DB820C" = TRUE ]; then
+    ./Dragonboard820cPkg/Tools/build-lkimage.ps1
+  elif [ "$LUMIA950XL" = TRUE ]; then
+    ./Lumia950XLPkg/Tools/build-lkimage.ps1
+  else
+    ./DragonboardPkg/Tools/build-lkimage.ps1
+  fi
+  
+}
+
+I=$#
+while [ $I -gt 0 ]
+do
+  case "$1" in
+    --development|-dev)
+      # Development build
+      echo "[Builder] Configure environment and run development build."
+      if [ "$CIBUILD" = TRUE ]; then
+        echo "[Builder] Only one build configuration can be selected."
+        return 1
+      else
+        DEVBUILD=TRUE
+      fi
+      shift
+    ;;
+    --production|-ci)
+      # CI Build
+      echo "[Builder] Configure environment and run CI build (clean)."
+      if [ "$DEVBUILD" = TRUE ]; then
+        echo "[Builder] Only one build configuration can be selected."
+        return 1
+      else
+        CIBUILD=TRUE
+      fi
+      shift
+    ;;
+    --820c|-820c)
+      # 820C build
+      echo "[Builder] Run Dragonboard 820c Build."
+      DB820C=TRUE
+      shift
+    ;;
+    --950xl|-950xl)
+      # 950XL build
+      echo "[Builder] Run Lumia 950 XL Build."
+      LUMIA950XL=TRUE
+      shift
+    ;;
+    -?|-h|--help|*)
+      HelpMsg
+      break
+    ;;
+  esac
+  I=$(($I - 1))
+done
+
+if [ $I -gt 0 ]
+then
+  return 1
+else
+
+  echo "[Builder] Configure environment."
+  SourceEnv
+  if [ ! $? -eq 0 ]; then
+      echo "Unable to configure EDK2 environment."
+      return $?
+  fi
+
+  # Run build
+  if [ "$DEVBUILD" = TRUE ]; then
+    DevelopmentBuild
+  elif [ "$CIBUILD" = TRUE ]; then
+    CIBuild
+  else
+    echo "[Builder] You must specify one build option."
+    return 1
+  fi
+
+  if [ ! $? -eq 0 ]; then
+      echo "[Builder] Build failed."
+      return $?
+  fi
+fi
