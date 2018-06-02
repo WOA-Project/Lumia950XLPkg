51a6b3a615a9dbdc3e05b2e5c05b0c797fffa8f0
Bingxing Wang <i@imbushuo.net>
2018-06-02 07:56:19 -0700

    MSM8992/Lumia 950: Add preliminary support.
    Special thanks to @nta (Bas Timmer), Jérémie Melchior and Kazker.
    Caution: Your mileage may vary.
    
    Starting from this commit, all EFI builds
    will also have commit stamped in SMBIOS.

--- Tools/edk2-build.ps1
+++ Tools/edk2-build.ps1
@@ -10,7 +10,13 @@ Param
 
 Write-Host "Task: EDK2 build"
 
-# Check path.
+# Targets. Ensure corresponding DSC/FDF files exist
+$availableTargets = @(
+	"Lumia950",
+	"Lumia950XL"
+)
+
+# Check package path.
 if ((Test-Path -Path "Lumia950XLPkg") -eq $null)
 {
     Write-Error "Lumia950XLPkg is not found."
@@ -19,7 +25,7 @@ if ((Test-Path -Path "Lumia950XLPkg") -eq $null)
 
 # Set environment again.
 Write-Output "[EDK2Build] Set environment."
-$env:PATH="/opt/db-boot-tools:/opt/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-elf/bin:/opt/gcc-linaro-7.2.1-2017.11-x86_64_arm-eabi/bin:/opt/signlk:/opt/skales:$($env:PATH)"
+$env:PATH="/opt/db-boot-tools:/opt/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-elf/bin:/opt/gcc-linaro-7.2.1-2017.11-x86_64_arm-eabi/bin:$($env:PATH)"
 $env:GCC5_AARCH64_PREFIX="/opt/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-elf/bin/aarch64-elf-"
 
 # Build base tools if not exist (dev).
@@ -36,23 +42,45 @@ if (((Test-Path -Path "BaseTools") -eq $false) -or ($Clean -eq $true))
 
 if ($Clean -eq $true)
 {
-    Write-Output "[EDK2Build] Clean Lumia950XLPkg."
-    build -a AARCH64 -p Lumia950XLPkg/Lumia950XL.dsc -t GCC5 clean
+	foreach ($target in $availableTargets)
+	{
+		Write-Output "[EDK2Build] Clean target $($target)."
+		build -a AARCH64 -p Lumia950XLPkg/$($target).dsc -t GCC5 clean
 
-    if (-not $?)
-    {
-        Write-Error "[EDK2Build] Clean target failed."
-        return $?
-    }
+		if (-not $?)
+		{
+			Write-Error "[EDK2Build] Clean target $($target) failed."
+			return $?
+		}
+	}
 
     # Apply workaround for "NUL"
-    Get-ChildItem -Path Build/**/NUL -Recurse | Remove-Item -Force
+	Get-ChildItem -Path Build/**/NUL -Recurse | Remove-Item -Force
 }
 
-Write-Output "[EDK2Build] Build Lumia950XLPkg (DEBUG)."
-build -a AARCH64 -p Lumia950XLPkg/Lumia950XL.dsc -t GCC5
-if (-not $?)
+# Check current commit ID and write it into file for SMBIOS reference.
+Write-Output "[EDK2Build] Stamp build."
+$commit = git rev-parse HEAD
+if ($commit)
 {
-    Write-Error "[EDK2Build] Build target failed."
-    return $?
+	$releaseInfoContent = @(
+		"#ifndef __SMBIOS_RELEASE_INFO_H__",
+		"#define __SMBIOS_RELEASE_INFO_H__",
+		"#define __IMPL_COMMIT_ID__ `"$($commit)`"",
+		"#endif"
+	)
+
+	Set-Content -Path Lumia950XLPkg/Driver/SmBiosTableDxe/ReleaseInfo.h -Value $releaseInfoContent -ErrorAction SilentlyContinue -Force
+}
+
+foreach ($target in $availableTargets)
+{
+	Write-Output "[EDK2Build] Build Lumia950XLPkg for $($target) (DEBUG)."
+	build -a AARCH64 -p Lumia950XLPkg/$($target).dsc -t GCC5
+
+	if (-not $?)
+	{
+		Write-Error "[EDK2Build] Build target $($target) failed."
+		return $?
+	}
 }
