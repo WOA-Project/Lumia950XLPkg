899b8c58b15ce0676daa72cd0e6cdb6ce6c2b7b4
Bingxing Wang <imbushuo@Septs-NMB.local>
2018-06-05 18:35:23 +0800

    Travis: Enable build.

---
+++ Tools/CI/Builder/Build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Travis CI builder
+#
+# Copyright 2018, Bingxing Wang. <i@imbushuo.net>
+# All rights reserved.
+#
+
+# Export AArch64 Tools prefix
+export GCC5_AARCH64_PREFIX=/opt/gcc-linaro-7.2.1-2017.11-x86_64_aarch64-elf/bin/aarch64-elf-
+
+# Go to EDK2 workspace
+cd edk2
+
+# Start build
+echo "Start build..."
+. rundbbuild.sh --950xl --development
