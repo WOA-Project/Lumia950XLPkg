8780d4c230b67d5b143077dce486f70d4d4acb84
mrmajik45 <35823544+mrmajik45@users.noreply.github.com>
2018-09-23 00:35:24 -0400

    Consider using strtok_r(...) instead of strtok(...) for improved thread safety.
    Much code has been originally written without consideration of multi-threading. Also, engineers are relying on their old experience; they have learned posix before threading extensions were added. This rule guide the engineers to use thread-safe functions (when using posix directly).

--- Include/Library/StrLib.h
+++ Include/Library/StrLib.h
@@ -1,7 +1,7 @@
 #ifndef __LIBRARY_STR_H__
 #define __LIBRARY_STR_H__
 
-CHAR8 *strtok(CHAR8 *s, CONST CHAR8 *delim);
+CHAR8 *strtok_r(CHAR8 *s, CONST CHAR8 *delim);
 CHAR8 *strtok_r(CHAR8 *s, CONST CHAR8 *delim, CHAR8 **last);
 UINTN strlcat(CHAR8 *dst, CONST CHAR8 *src, UINTN siz);
 UINTN strspn(CHAR8 CONST *s, CHAR8 CONST *accept);
