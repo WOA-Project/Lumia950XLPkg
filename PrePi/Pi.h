d26e91c09e7f1eb19c58e6ec2d82e26a02c3bc9b
Bingxing Wang <i@imbushuo.net>
2019-03-20 08:14:47 -0700

    PrePi: Standardized code styles.

--- PrePi/Pi.h
+++ PrePi/Pi.h
@@ -1,54 +1,45 @@
 /** @file
-*
-*  Copyright (c) 2011-2012, ARM Limited. All rights reserved.
-*
-*  This program and the accompanying materials
-*  are licensed and made available under the terms and conditions of the BSD License
-*  which accompanies this distribution.  The full text of the license may be found at
-*  http://opensource.org/licenses/bsd-license.php
-*
-*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
-*
-**/
+ *
+ *  Copyright (c) 2011-2012, ARM Limited. All rights reserved.
+ *
+ *  This program and the accompanying materials
+ *  are licensed and made available under the terms and conditions of the BSD
+ *License which accompanies this distribution.  The full text of the license may
+ *be found at http://opensource.org/licenses/bsd-license.php
+ *
+ *  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR
+ *IMPLIED.
+ *
+ **/
 
 #ifndef _PREPI_H_
 #define _PREPI_H_
 
 #include <PiPei.h>
 
-#include <Library/PcdLib.h>
 #include <Library/ArmLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/DebugLib.h>
+#include <Library/HobLib.h>
 #include <Library/IoLib.h>
 #include <Library/MemoryAllocationLib.h>
-#include <Library/HobLib.h>
+#include <Library/PcdLib.h>
 #include <Library/SerialPortLib.h>
 
 EFI_STATUS
 EFIAPI
-MemoryPeim(
-  IN EFI_PHYSICAL_ADDRESS       UefiMemoryBase,
-  IN UINT64                     UefiMemorySize
-);
+MemoryPeim(IN EFI_PHYSICAL_ADDRESS UefiMemoryBase, IN UINT64 UefiMemorySize);
 
 EFI_STATUS
 EFIAPI
-PlatformPeim(
-  VOID
-);
+PlatformPeim(VOID);
 
 // Either implemented by PrePiLib or by MemoryInitPei
-VOID
-BuildMemoryTypeInformationHob(
-  VOID
-);
+VOID BuildMemoryTypeInformationHob(VOID);
 
 EFI_STATUS
 EFIAPI
-QGicPeim(
-	VOID
-);
+QGicPeim(VOID);
 
 #endif /* _PREPI_H_ */
