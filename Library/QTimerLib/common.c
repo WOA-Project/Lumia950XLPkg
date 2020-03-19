83cf78f2c60658ee7e9f43b25c037328089bf4cb
Bingxing Wang <uefi-oss-projects@imbushuo.net>
2020-03-19 04:40:11 -0400

    Timer: introduce QTimer in UEFI
    This also resolves #62, which is a hardware bug in Qualcomm's armv8 arch timer implementation.

---
+++ Library/QTimerLib/common.c
@@ -0,0 +1,18 @@
+#include <Base.h>
+#include <Library/ArmGenericTimerCounterLib.h>
+
+UINT32 qtimer_get_frequency(VOID)
+{
+  UINT32 freq;
+
+  /* Read the Global counter frequency */
+  /* freq = readl(QTMR_V1_CNTFRQ); */
+  /* TODO: remove this when bootchaint sets up the frequency. */
+  freq = 19200000;
+
+  return freq;
+}
+
+UINTN
+EFIAPI
+ArmGenericTimerGetTimerFreq(VOID) { return qtimer_get_frequency(); }
