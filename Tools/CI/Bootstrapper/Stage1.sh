86b48b87fcddc253e9f7702bb7c09841f45db703
Gustave Monce <gustave.monce@outlook.com>
2025-12-14 10:21:10 +0100

    Pin specific edk2 commit

--- Tools/CI/Bootstrapper/Stage1.sh
+++ Tools/CI/Bootstrapper/Stage1.sh
@@ -10,7 +10,16 @@
 echo "Checking out EDK2 workspace"
 
 cd ..
-git clone --single-branch --depth 1 --recurse-submodules --branch master https://github.com/tianocore/edk2
+#git clone --single-branch --depth 1 --recurse-submodules --branch master https://github.com/tianocore/edk2
+# Checkout 838c730fe61578847823da1b815e4c9c5ee1bed4 (LKG)
+# We do not use the revision param for compatibility with older git versions
+mkdir edk2
+cd edk2
+git init
+git remote add origin https://github.com/tianocore/edk2
+git fetch --depth=1 origin 838c730fe61578847823da1b815e4c9c5ee1bed4
+git checkout FETCH_HEAD
+cd ..
 
 # Set a link to EDK2 workspace
 ln -s $(pwd)/Lumia950XLPkg $(pwd)/edk2/Lumia950XLPkg
